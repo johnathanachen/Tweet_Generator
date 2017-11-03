8cfefe4da8e28c5c58cb95131372e1041145d00e
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-03 10:48:13 -0700

    jfkd

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Johnathan/Desktop/Makeschool/Class Assignments/CS-2-Tweet-Generator/app/venv"
+set -gx VIRTUAL_ENV "/Users/Johnathan/Desktop/app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
