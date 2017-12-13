d429b46a042d63243ff536b84dd816d83b833de6
Johnathan Chen <johnathanchenn@outlook.com>
2017-12-13 15:51:46 -0800

    ready to present

--- scripts/markov.py
+++ scripts/markov.py
@@ -1,59 +1,213 @@
-from histograms import Dictogram
-import random
+from scripts.dictogram import Dictogram
 from collections import deque
-import re
+import codecs
 
-class Markov():
+class MarkovChain:
 
-    def __init__(self):
-        pass
+    # Max amount of attempts we have to generate a new sentence
+    MAX_ITERATION_ATTEMPTS = 100
 
-    def _markov_chain(self, data):
-        """ Markov model """
-        #Dictionary that stores windows as the key in the key-value pair and then the value
-        #for each key is a dictogram
-        markov_chain = dict()
-        # Looping through the ammount of indexs in the list
-        for index in range(0, len(data) - 1):
-            # If index word of list exists in dictionary then update the current index
-            # Store a histogram of words for each window
-            if data[index] in markov_chain:
-                markov_chain[data[index]].update([data[index + 1]])
+    def __init__(self, corpus, max_order):
+        """ Constructing the Markov Model
+        :param corpus:      The file location of the corpus we want to use
+        :param order:       The order for the Markov Chain
+        """
+
+        self.parser = FileParser(corpus)
+        self.dictogram = []
+        self.max_order = max_order
+
+        for order in range(1, max_order + 1):
+            print("START: " + str(order))
+            self.dictogram.append(Dictogram(self.parser.words, order))
+            print("END: " + str(order))
+
+
+    def generate_sentence(self, backward=False, min_length=50, max_length=200):
+        """ Generates a sentence by first getting a sentence start then getting a random token following that word. We
+        then end the sentence once we get to an end token ([NONE]). If the sentence is too small or too long we try
+        generating a sentence gain.
+        :return:    Our uniquely generated sentence
+        """
+
+        element = None
+        generated_sentence = ''
+
+        if backward:
+            window = deque(self.dictogram[self.max_order - 1].random_start(True))
+            element = self.dictogram[self.max_order - 1].backwards[tuple(window)]
+            generated_sentence = ' '.join(list(reversed(window)))
+        else:
+            window = deque(self.dictogram[self.max_order - 1].random_start(False))
+            element = self.dictogram[self.max_order - 1].forwards[tuple(window)]
+            generated_sentence = ' '.join(window)
+
+        # If the window has a split
+        if '[SPLIT]' in generated_sentence:
+            return self.generate_sentence(backward)
+
+        # We could use a while loop, but we do this instead because we want to make sure we never get an infinite loop
+        for _ in range(self.MAX_ITERATION_ATTEMPTS):
+            word = None
+
+            # We get a new word or phrase
+            current_word = element.random_word()
+
+            # If the word is a sentence end, we finish off the sentence.
+            if current_word == '[SPLIT]':
+                if not backward:
+                    generated_sentence += '.'
+
+                break
+
+            # We make the new word or phrase our current element
+            window.popleft()
+            window.append(current_word)
+
+            if backward:
+                element = self.dictogram[self.max_order - 1].backwards[tuple(window)]
+
+                # We only use the first word in the phrase
+                word = current_word + " "
+
+                # Add current word to our new sentence
+                generated_sentence = word + generated_sentence
             else:
-                markov_chain[data[index]] = Dictogram([data[index + 1]])
-        return markov_chain
+                element = self.dictogram[self.max_order - 1].forwards[tuple(window)]
 
-    # Walk our model
-    def _generate_random_start(self, model):
-        # Generate a "valid" starting word.
-        # A valid starting word are words that start a sentence
-        return random.choice(list(model.keys()))
+                word = " " + current_word
 
-    # Generating sentence using first order markov_model
-    def _generate_sentence(self, length, markov_model):
-        # length parameter is length of the sentence
-        # Create first word
-        current_word = self._generate_random_start(markov_model)
-        # Save first word to sentence list
-        sentence = [current_word]
-        # Loop through the length of sentence provided
-        for i in range(0, length):
-            # Getting current dictogram and starting from the current word(first word)
-            current_dictogram = markov_model[current_word]
-            # Getting random word from dictogram starting from the place of the current word
-            random_word = current_dictogram.return_weighted_random_word()
-            # Setting current word variable to the random word
-            current_word = random_word
-            # Append the new current word until the sentence length is formed
-            sentence.append(current_word)
-        sentence[0] = sentence[0].capitalize()
-        sentence = ' '.join(sentence) + '.'
-        return sentence
+                generated_sentence += word
 
-    def main(self, data, length):
-        markov_chain = self._markov_chain(data)
-        sentence = self._generate_sentence(length, markov_chain)
-        return sentence
+        sentence_length = len(generated_sentence)
 
-# data = ["how", "much", "wood", "would", "a", "wood", "chuck", "chuck", "if", "a", "wood", "chuck", "could", "chuck", "wood"]
-# Markov(data, 10)
+        # If our sentence is too short or long we return a new sentence
+        if sentence_length > max_length or sentence_length < min_length:
+            return self.generate_sentence(backward)
+
+        # If we are backwards add a period at the end of a sentence
+        if backward:
+            generated_sentence += '.'
+
+        # Return our sentence and capitalize it. Also make sure there are no uncalled for None tokens
+        return generated_sentence
+
+    def generate_with_seed(self, raw_seed, min_length=50, max_length=200, depth=0):
+        seed = list(raw_seed.split())
+
+        while len(seed) < self.max_order:
+            try:
+                word = self.dictogram[len(seed) - 1].forwards[tuple(seed)].random_word()
+
+                if word == '[SPLIT]':
+                    return self.generate_with_seed(raw_seed)
+
+                seed.append(word)
+            except KeyError:
+                return None
+
+        seed = tuple(seed)
+
+        if seed in self.dictogram[self.max_order - 1].forwards and tuple(reversed(seed)) in self.dictogram[
+                    self.max_order - 1].backwards:
+            forward_element = self.dictogram[self.max_order - 1].forwards[seed]
+            forward_window = deque(seed)
+            forward_sentence = ''
+
+            for _ in range(self.MAX_ITERATION_ATTEMPTS):
+                word = None
+
+                # We get a new word or phrase
+                current_word = forward_element.random_word()
+
+                # If the word is a sentence end, we finish off the sentence.
+                if current_word == '[SPLIT]':
+                    forward_sentence += '.'
+                    break
+
+                # We make the new word or phrase our current element
+                forward_window.popleft()
+                forward_window.append(current_word)
+
+                forward_element = self.dictogram[self.max_order - 1].forwards[tuple(forward_window)]
+                word = " " + current_word
+                forward_sentence += word
+
+            forward_length = len(forward_sentence)
+
+            # If our sentence is too short or long we return a new sentence
+            if forward_length > max_length / 2 or forward_length < min_length / 2:
+                if depth > self.MAX_ITERATION_ATTEMPTS:
+                    return None
+
+                return self.generate_with_seed(raw_seed, depth=depth + 1)
+
+            backward_window = deque(tuple(reversed(seed)))
+            backward_element = self.dictogram[self.max_order - 1].backwards[tuple(reversed(seed))]
+            backward_sentence = ' '.join(list(reversed(backward_window)))
+
+            # If the window has a split
+            if '[SPLIT]' in backward_sentence:
+                if depth > self.MAX_ITERATION_ATTEMPTS:
+                    return None
+
+                return self.generate_with_seed(raw_seed, depth=depth + 1)
+
+            for _ in range(self.MAX_ITERATION_ATTEMPTS):
+                word = None
+
+                # We get a new word or phrase
+                current_word = backward_element.random_word()
+
+                # If the word is a sentence end, we finish off the sentence.
+                if current_word == '[SPLIT]':
+                    break
+
+                # We make the new word or phrase our current element
+                backward_window.popleft()
+                backward_window.append(current_word)
+
+                backward_element = self.dictogram[self.max_order - 1].backwards[tuple(backward_window)]
+                word = current_word + " "
+                backward_sentence = word + backward_sentence
+
+            backward_length = len(backward_sentence)
+
+            # If our sentence is too short or long we return a new sentence
+            if backward_length > max_length / 2 or backward_length < min_length / 2:
+                if depth > self.MAX_ITERATION_ATTEMPTS:
+                    return None
+
+                return self.generate_with_seed(raw_seed, depth=depth + 1)
+
+            return backward_sentence + forward_sentence
+
+
+
+class FileParser:
+    """ FileParser is a class used to parse both lines and words from a file. I will also sterilize the data """
+
+    # TODO: When cleaning lines. Check that it's not a whitelisted use of a period
+
+    def __init__(self, file_name):
+        """ We read a file named :file_name: and put all of the words and lines into a variable
+        :param file_name:       The name of the file we want to parse
+        """
+
+        file_reader = codecs.open(file_name, "r", "utf8")
+
+        self.words = []
+        self.lines = file_reader.read().splitlines()
+
+        # Goes through all the lines, cleans them, then splits it up into words and adds them to the words variable
+        for line in self.lines:
+            self.words += line.split()
+
+    @staticmethod
+    def clean_line(line):
+        """ We clean a line. At the moment we just add the [NONE] token at the end of sentences.
+        :param line:    The line we want to clean
+        :return:        The cleaned line
+        """
+
+        return line.replace('.', ' [NONE]')
