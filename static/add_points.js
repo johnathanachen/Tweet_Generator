6276bb27d02bd0b23427a5149dd245ee07675d72
Johnathan Chen <johnathanchenn@outlook.com>
2017-12-14 15:10:19 -0800

    change to 2 buttons

--- static/add_points.js
+++ static/add_points.js
@@ -1,21 +1,8 @@
-// $('.real-btn').on('click', function() {
-//
-//   $('<span class="plus-one"/>', {
-//       style: 'display:none'
-//     })
-//     .html('+10')
-//     .appendTo($('.add-comment'))
-//     .fadeIn('1000', function() {
-//       var el = $(this);
-//       setTimeout(function() {
-//         el.remove();
-//       }, 2000);
-//     });
+// $('.middle-btn').on('click', function() {
 //
 //     var text = $(this).val();
-//
 //     $.ajax({
-//       url: "/new_text",
+//       url: "/pick_text",
 //       type: "get",
 //       data: {text},
 //       success: function(response) {
@@ -27,64 +14,17 @@
 //     });
 //
 //     var countText = $(this).val();
-//
 //     $.ajax({
 //       url: "/new_count",
 //       type: "get",
 //       data: {countText},
 //       success: function(response) {
 //         $(".new-count").html(response);
-//       },
-//       error: function(xhr) {
-//         //Do Something to handle error
 //       }
 //     });
-//
+// document.getElementById("button").disabled = false;
+// document.getElementById("button2").disabled = false;
 // });
-//
-// $('.fake-btn').on('click', function() {
-//
-//   $('<span class="minus-one"/>', {
-//       style: 'display:none'
-//     })
-//     .html('-10')
-//     .appendTo($('.add-comment'))
-//     .fadeIn('1000', function() {
-//       var el = $(this);
-//       setTimeout(function() {
-//         el.remove();
-//       }, 2000);
-//     });
-//
-// });
-
-$('.middle-btn').on('click', function() {
-
-    var text = $(this).val();
-    $.ajax({
-      url: "/pick_text",
-      type: "get",
-      data: {text},
-      success: function(response) {
-        $(".tweet--body").html(response);
-      },
-      error: function(xhr) {
-        //Do Something to handle error
-      }
-    });
-
-    var countText = $(this).val();
-    $.ajax({
-      url: "/new_count",
-      type: "get",
-      data: {countText},
-      success: function(response) {
-        $(".new-count").html(response);
-      }
-    });
-document.getElementById("button").disabled = false;
-document.getElementById("button2").disabled = false;
-});
 
 function read_score(countText) {
     $.ajax({
@@ -164,8 +104,31 @@ $('.real-btn').on('click', function() {
          }
       }
     });
-document.getElementById("button").disabled = true;
-document.getElementById("button2").disabled = true;
+
+    var text = $(this).val();
+    $.ajax({
+      url: "/pick_text",
+      type: "get",
+      data: {text},
+      success: function(response) {
+        $(".tweet--body").html(response);
+      },
+      error: function(xhr) {
+        //Do Something to handle error
+      }
+    });
+
+    var countText = $(this).val();
+    $.ajax({
+      url: "/new_count",
+      type: "get",
+      data: {countText},
+      success: function(response) {
+        $(".new-count").html(response);
+      }
+    });
+// document.getElementById("button").disabled = true;
+// document.getElementById("button2").disabled = true;
 });
 
 
@@ -214,8 +177,31 @@ $('.fake-btn').on('click', function() {
          }
       }
     });
-document.getElementById("button").disabled = true;
-document.getElementById("button2").disabled = true;
+
+    var text = $(this).val();
+    $.ajax({
+      url: "/pick_text",
+      type: "get",
+      data: {text},
+      success: function(response) {
+        $(".tweet--body").html(response);
+      },
+      error: function(xhr) {
+        //Do Something to handle error
+      }
+    });
+
+    var countText = $(this).val();
+    $.ajax({
+      url: "/new_count",
+      type: "get",
+      data: {countText},
+      success: function(response) {
+        $(".new-count").html(response);
+      }
+    });
+// document.getElementById("button").disabled = true;
+// document.getElementById("button2").disabled = true;
 
 
 
